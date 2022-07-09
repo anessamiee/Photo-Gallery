d72ccb17cd5558d2af238c31063b3866f91c533b
anessamiee <anessamiee@gmail.com>
2022-07-09 21:58:50 +0430

    functions created

---
+++ index.js
@@ -0,0 +1,8 @@
+const setName = () => {
+  let text = document.querySelector("#input").value;
+  document.getElementById("header-title").textContent = text;
+};
+// prevent reload on submit
+let form = document.getElementById("form");
+const handleForm = (event) => event.preventDefault();
+form.addEventListener("submit", handleForm);
